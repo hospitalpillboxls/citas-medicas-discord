53837dbd19cbd5e649fc3df8b3b3b488d76537f2
hospitalpillboxls <hospitalpillboxls@gmail.com>
2026-08-15 17:33:24 +0200

    availability.js

---
+++ netlify/functions/availability.js
@@ -0,0 +1,10 @@
+medical-dating-discord/
+│
+├── index.html
+├── netlify.toml
+│
+└── netlify/
+    └── functions/
+        ├── availability.js   ← AQUÍ
+        ├── book.js           ← AQUÍ
+        └── cita.js
